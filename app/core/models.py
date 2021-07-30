c782957b2120c5207b211648f95d8ba1c967d861
nandinichhajed <nandinichhajed08@gmail.com>
2021-07-30 21:37:40 +0530

    Added create tag feature

--- app/core/models.py
+++ app/core/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
-
+from django.conf import settings 
 
 class UserManager(BaseUserManager):
     def create_user(self, email, password = None, **extra_fields):
@@ -22,7 +22,6 @@ class UserManager(BaseUserManager):
 
         return user
 
-
 class User (AbstractBaseUser, PermissionsMixin):
     # custom user model that support email instead of username
     email = models.EmailField(max_length=255, unique = True)
@@ -33,3 +32,14 @@ class User (AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
+
+class Tag(models.Model):
+    """Tag to be used for a recipe"""
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.name
