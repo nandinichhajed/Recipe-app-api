10e8e04c41bf4b5218bf70dc1c2b773d30bc7b52
nandinichhajed <nandinichhajed08@gmail.com>
2021-07-30 18:07:48 +0530

    Added manage user token

--- app/user/views.py
+++ app/user/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 
-from rest_framework import generics
+from rest_framework import generics, authentication,permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 
 from user.serializers import UserSerializer, AuthTokenSerializer
-from user.serializers import UserSerializer
+
 
 
 class CreateUserView(generics.CreateAPIView):
@@ -17,3 +17,12 @@ class CreateTokenView(ObtainAuthToken):
     """Create a new auth token for the user"""
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authenticated user"""
+    serializer_class = UserSerializer
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user    
