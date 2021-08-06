e7f69fa4d42a2352b067fa58b44f9955c33fd3fb
nandinichhajed <nandinichhajed08@gmail.com>
2021-08-06 16:52:19 +0530

    Implemented feature of filtering Tags and Ingredient

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -5,7 +5,7 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 
 
-from core.models import Tag, Ingredient,Recipe
+from core.models import Tag, Ingredient, Recipe
 
 from recipe import serializers
 
@@ -18,7 +18,16 @@ class BaseRecipeAttrViewSet(viewsets.GenericViewSet,
 
     def get_queryset(self):
         """Return objects for the current authenticated user only"""
-        return self.queryset.filter(user=self.request.user).order_by('-name')
+        assigned_only = bool(
+            int(self.request.query_params.get('assigned_only', 0))
+        )
+        queryset = self.queryset
+        if assigned_only:
+            queryset = queryset.filter(recipe__isnull=False)
+
+        return queryset.filter(
+            user=self.request.user
+        ).order_by('-name').distinct()
 
     def perform_create(self, serializer):
         """Create a new object"""
